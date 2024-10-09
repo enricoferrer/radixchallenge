71b13cc35741d08941090582ec4f3a4950542347
enricoferrer <enricoferrer@outlook.com>
2024-10-09 12:06:22 -0300

    Arquivo CSV para JSON inserindo no banco

--- challenge/src/Dashboard.jsx
+++ challenge/src/Dashboard.jsx
@@ -1,8 +1,67 @@
 import React, { useEffect, useState } from "react";
+import Papa from "papaparse";
 
 const Dashboard = () => {
-  const [dadosJSON, setDadosJSON] = useState([]); // Estado para armazenar os dados do JSON
+  const [dadosJSON, setDadosJSON] = useState([]); 
+  const [dadosCSV, setDadosCSV] = useState([])
 
+  
+  const lerArquivoCSV = (event) => {
+    const arquivo = event.target.files[0];
+    if (arquivo) {
+      Papa.parse(arquivo, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (resultado) => {
+          const dadosFormatados = resultado.data.map(item => ({
+            equipmentId: item.equipmentId,
+            timestamp: new Date(item.timestamp).toISOString(), 
+            value: parseFloat(item.value),
+          }));
+  
+          console.log("CSV lido com sucesso:", dadosFormatados);
+          setDadosCSV(dadosFormatados); 
+        },
+      });
+    }
+  };
+
+  useEffect(() => {
+    console.log("Dados CSV atualizados:", dadosCSV);
+  }, [dadosCSV]);
+
+  async function enviarCSVParaGraphQL() {
+    const requestBody = {
+      query: `
+            mutation CreateEquipment($input: [EquipmentInput]!) {
+                createEquipment(input: $input) {
+                    equipmentId
+                    timestamp
+                    value
+                }
+            }           
+        `,
+      variables: {
+        input: dadosCSV, 
+      },
+    };
+
+    const inserirEquipamento = await fetch("http://localhost:9000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(dadosCSV)
+
+    if (inserirEquipamento.ok) {
+      alert("Inserção realizada com sucesso!");
+    } else {
+      alert("Erro ao realizar a inserção.");
+    }
+  }
+  
   useEffect(() => {
     const caminhoDoArquivo = "/equipment.JSON";
 
@@ -14,8 +73,7 @@ const Dashboard = () => {
         return response.json();
       })
       .then((dados) => {
-        console.log(dados);
-        setDadosJSON(dados); // Armazenar os dados no estado
+        setDadosJSON(dados); 
       })
       .catch((err) => {
         console.error("Erro ao ler o arquivo JSON:", err);
@@ -34,7 +92,7 @@ const Dashboard = () => {
             }           
         `,
       variables: {
-        input: dadosJSON, // Usar os dados do estado
+        input: dadosJSON,
       },
     };
 
@@ -55,7 +113,9 @@ const Dashboard = () => {
 
   return (
     <div>
+      <input type="file" accept=".csv" onChange={lerArquivoCSV} />
       <button onClick={enviarDadosParaGraphQL}>Enviar Dados</button>
+      <button onClick={enviarCSVParaGraphQL}>Enviar CSV</button>
     </div>
   );
 };
